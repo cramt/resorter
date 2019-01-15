37a9dbbcbdc74cc068f644e5b146f759308173fb
mads østermark <madscramt@gmail.com>
2019-01-15 12:56:59 +0100

    SplitWithin works

--- resorter/Program.cs
+++ resorter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,6 +12,9 @@ namespace resorter {
         /// </summary>
         [STAThread]
         static void Main() {
+            var a = "\"your,mom\",23,[2,3,4]".SplitWithin(',', new char[][] { new char[] { '\"', '\"' }, new char[] { '[', ']' } });
+            a.ForEach(Console.WriteLine);
+            Thread.Sleep(-1);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
---
+++ resorter/Utilities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resorter {
+    public static class Utilities {
+        public static List<string> SplitWithin(this string str, char splittingChar, char[][] p) {
+            bool[] triggeredP = new bool[p.Length];
+            List<int> splitPointsList = new List<int>();
+            splitPointsList.Add(-1);
+            for (int i = 0; i < str.Length; i++) {
+                for (int j = 0; j < p.Length; j++) {
+                    if (p[j][0] == p[j][1]) {
+                        if (p[j][0] == str[i]) {
+                            triggeredP[j] = !triggeredP[j];
+                        }
+                    }
+                    else {
+                        if (p[j][0] == str[i]) {
+                            triggeredP[j] = true;
+                        }
+                        if (p[j][1] == str[i]) {
+                            triggeredP[j] = false;
+                        }
+                    }
+                }
+                if (!triggeredP.Any(x => x)) {
+                    if (str[i] == splittingChar) {
+                        splitPointsList.Add(i);
+                    }
+                }
+            }
+            splitPointsList.Add(str.Length);
+            int[] splitPoints = splitPointsList.ToArray();
+            List<string> re = new List<string>();
+            for (int i = 0; i < splitPoints.Length - 1; i++) {
+                int a = splitPoints[i] + 1;
+                int b = splitPoints[i + 1];
+                re.Add(str.Substring(a, b - a));
+            }
+            return re;
+        }
+    }
+}
